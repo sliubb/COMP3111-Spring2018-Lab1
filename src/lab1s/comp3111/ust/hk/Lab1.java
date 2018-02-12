0694cd899486cb0dc38d5a731a9c46fc0b3693db
summer <summer@10.89.71.243>
2018-02-12 12:43:37 +0800

    Added a congratulation message

--- src/lab1s/comp3111/ust/hk/Lab1.java
+++ src/lab1s/comp3111/ust/hk/Lab1.java
@@ -4,5 +4,6 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		System.out.println("Lab1: Introduction to Git and GitHub");
+		System.out.println("Congratulation! you learned the Git Basics");
 	}
 }
